f9f0c3bcc3a929be56fd32aed7b7bc4eae3992e7
jurvantoledo <jurvantoledo@live.nl>
2020-11-02 20:37:51 +0100

    Extended the store and added css

--- src/components/PizzaList.js
+++ src/components/PizzaList.js
@@ -1,37 +1,60 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import "./PizzaList.css";
 
 const selectUser = (reduxState) => {
   return reduxState.user;
 };
 
-const selectPizza = (reduxState) => {
-  return reduxState.pizzas.sort((a, b) => {
+const selectPizzas = (reduxState) => {
+  return reduxState.pizzas.slice().sort((a, b) => {
     return b.bought - a.bought;
   });
 };
 
 export default function PizzaList() {
+  const dispatch = useDispatch();
   const user = useSelector(selectUser);
-  const pizzas = useSelector(selectPizza);
+  const pizzas = useSelector(selectPizzas);
 
   return (
-    <div>
+    <div className="PizzaList">
       <h1>Pizza Explorer</h1>
       <p>
-        Welcome back, <strong>{user.name}</strong>! Your favorite pizzas
+        Welcome back, <strong>{user.name}</strong>! You have{" "}
+        {user.favorites.length} favorite pizzas:
       </p>
-      <p>Your list of pizza's</p>
-      <ul>
-        {pizzas.map((pizza) => (
-          <li key={pizza.id}>
-            <p>{pizza.name}</p>
-            <p>{pizza.description}</p>
-            <p>People bought: {pizza.bought} of these</p>
-          </li>
-        ))}
+      <ul className="pizzas">
+        {pizzas.map((pizza) => {
+          const toggle = () => {
+            dispatch({
+              type: "TOGGLE_FAVORITE_PIZZA",
+              payload: pizza.id,
+            });
+          };
+
+          return (
+            <li
+              key={pizza.id}
+              className="pizza"
+              style={{ backgroundImage: `url(${pizza.image})` }}
+            >
+              <button
+                onClick={toggle}
+                className={`FavToggle ${
+                  user.favorites.includes(pizza.id) ? "fav" : ""
+                }`}
+              >
+                {user.favorites.includes(pizza.id) ? "♥" : "♡"}
+              </button>
+              <div className="Overlay">
+                <strong>{pizza.name}</strong> ({pizza.description}) <br />
+                <em>Bought {pizza.bought} times</em>
+              </div>
+            </li>
+          );
+        })}
       </ul>
-      <p>TODO: the list of pizza's</p>
     </div>
   );
 }
