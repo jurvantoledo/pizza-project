d86ba2f3f42b27c54996b2068bd2104efa8d9d62
jurvantoledo <jurvantoledo@live.nl>
2020-11-02 20:38:46 +0100

    Extended the store and added css

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,6 +2,7 @@
 const initialState = {
   user: {
     name: "Helva",
+    favorites: [161235, 357311],
   },
   pizzas: [
     {
@@ -10,6 +11,8 @@ const initialState = {
       description:
         "The typical Neapolitan pizza, made with San Marzano tomatoes, mozzarella cheese, fresh basil, salt and extra-virgin olive oil.",
       bought: 5,
+      image:
+        "https://imgp2.schaer.com/sites/default/files/styles/header/public/2017-09/HeaderProducts_Pizza%20Margherita.jpg?itok=Fz-7_a56",
     },
     {
       id: 67283,
@@ -17,6 +20,8 @@ const initialState = {
       description:
         "Neapolitan pizza also known as Naples-style pizza, is a style of pizza made with tomatoes and mozzarella cheese.",
       bought: 2,
+      image:
+        "https://uncutrecipes.com/Images-Recipes-Italian/Pizza-alla-Napoletana-with-Mozzarella-Cheese.jpg",
     },
     {
       id: 357311,
@@ -24,6 +29,8 @@ const initialState = {
       description:
         "White pizza, which omits tomato sauce from the equation, often substituting it with pesto or sour cream.",
       bought: 10,
+      image:
+        "https://img.static-rmg.be/a/food/image/q100/w480/h360/1087722/pizza-bianca-met-artisjok-en-mortadella.jpg",
     },
   ],
 };
@@ -43,6 +50,23 @@ export default function reducer(state = initialState, action) {
             bought: 0,
           },
         ],
+      };
+    }
+    case "TOGGLE_FAVORITE_PIZZA": {
+      const id = action.payload;
+      let favorites = state.user.favorites.slice();
+      if (favorites.includes(id)) {
+        favorites.splice(favorites.indexOf(id), 1);
+      } else {
+        favorites.push(id);
+      }
+
+      return {
+        ...state,
+        user: {
+          ...state.user,
+          favorites,
+        },
       };
     }
     default: {
